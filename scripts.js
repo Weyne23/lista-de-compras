76903b67770b7ee264a014c3f81945163a5eabb2
Weyne <augustoweyne12@gmail.com>
2025-05-08 18:38:44 -0300

    FInalizado projeto

--- scripts.js
+++ scripts.js
@@ -1,16 +1,58 @@
 const form = document.querySelector("form");
 const newItem = document.getElementById("new-item");
-const listaCompras = document.getElementById("buy-list");
+const shoppingList = document.getElementById("shopping-list");
+const msgAlert = document.querySelector(".alert");
+const closeAlert = document.getElementById("close-alert");
 
 form.onsubmit = (event) => {
     event.preventDefault();
-    if (newItem.value === ""){
-        return alert("Escreva ")
-    }
-    newItem.value = "TESTE";
-    console.log(newItem.value);
+    if (newItem.value === "")
+        return alert("Digite um item!")
+
+    addNewItem(newItem.value);
 }
 
-function addNewItem() {
+closeAlert.addEventListener("click", () => {
+    msgAlert.classList.add("opacity-0");
+})
 
+/**
+ * Add item in shopping list
+ * @param {String} item 
+ */
+function addNewItem(item) {
+    const newLiItem = document.createElement("li");
+    const newDiv = document.createElement("div");
+    const newInnerDiv = document.createElement("div");
+    const newInput = document.createElement("input");
+    const newLabel= document.createElement("label");
+    const newSpan= document.createElement("span");
+    const newImg= document.createElement("img");
+
+    newImg.setAttribute("src", "./assets/icons/Frame.svg");
+    newImg.setAttribute("alt", "Lata de lixo");
+    newImg.setAttribute("for", item);
+    newInnerDiv.classList.add("flex", "align-items-center");
+    newDiv.classList.add("flex", "align-items-center", "box-style");
+    newLabel.textContent = item;
+    newLabel.setAttribute("for", item);
+    newInput.setAttribute("type", "checkbox");
+    newInput.setAttribute("name", item);
+    newInput.setAttribute("id", item);
+    newInput.setAttribute("value", item);
+    newSpan.append(newImg);
+    newInnerDiv.append(newInput, newLabel);
+    newDiv.append(newInnerDiv, newSpan);
+    newLiItem.append(newDiv);
+    shoppingList.append(newLiItem);
+
+    newImg.addEventListener("click",() => deleteItemList(newLiItem));
+}
+
+function deleteItemList(item) {
+    item.remove();
+    msgAlert.classList.remove("opacity-0");
+    setTimeout(() => {
+        msgAlert.classList.add("opacity-0");
+    }, 2000);
 }
